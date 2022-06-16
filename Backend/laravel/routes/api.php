2b2b3104e0b95a84da2c8ab01a2f2ccc3ed95ec2
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-16 20:57:05 +0300

    Initial Commit

---
+++ Backend/laravel/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
