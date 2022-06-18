773491ff07a93792d6ae4add5123064e61fc697c
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-19 02:30:13 +0300

    Added Link to Populate Surveys

--- Frontend/astro-chimp/src/components/AddSurveyPage.js
+++ Frontend/astro-chimp/src/components/AddSurveyPage.js
@@ -8,6 +8,11 @@ const AddSurveyPage = () => {
             <NavBar/>
             <div className="add-survey-page">
                 <button className="add-btn">Add Survey</button>
+                <select>
+                <option>haahahahahah</option>
+                <option>HI</option>
+                <option>Hello</option>
+            </select>
             </div>
         </div>
     )
--- Frontend/astro-chimp/src/components/SurveyPage.js
+++ Frontend/astro-chimp/src/components/SurveyPage.js
@@ -5,22 +5,31 @@ import SurveyElement from "./SurveyElement";
 
 
 const SurveyPage = () => {
+    function createSurvey(surveyName){
+        var div = document.createElement("div");
+        div.setAttribute("class", "survey-box");
+        div.innerText = surveyName;
+        var div2 = document.getElementsByClassName("survey-container");
+        div2[0].appendChild(div)
+    }
     
     return(
-        function addSurvey(){
-            axios({
-                method: "GET",
-                url: "http://127.0.0.1:8000/api/get-surveys",
-            })
+        
+        axios({
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/get-surveys",
+        })
     
-            .then(function (response) {
-                console.log(response.data)
-                var data = response.data["surveys"]
-                for (var i = 0; i < data.length; i++){
-                    console.log(data[i]["name"])
-                }
-            })
-        }, 
+        .then(function (response) {
+            console.log(response.data)
+            var data = response.data["surveys"]
+            for (var i = 0; i < data.length; i++){
+                var surveyName = data[i]["name"]
+                createSurvey(surveyName)
+            }
+        }),
+        
+
         <div>
             <NavBar/>
             <h1 className="page-title">Surveys</h1>
