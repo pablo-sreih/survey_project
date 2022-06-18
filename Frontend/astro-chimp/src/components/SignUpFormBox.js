e4f3d56842ff27f747eb88dd731c9f484b9f7034
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-18 20:25:01 +0300

    Added Sign Up Linking with Axios

--- Frontend/astro-chimp/src/components/SignUpFormBox.js
+++ Frontend/astro-chimp/src/components/SignUpFormBox.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SignUpFormBox = () => {
     function signUp(){
-        console.log("SignUp Clicked")
+        var name = document.getElementById("name")
+        var email = document.getElementById("email")
+        var password = document.getElementById("password")
+
+        let data = new FormData
+        data.append("name", name.value)
+        data.append("email", email.value)
+        data.append("password", password.value)
+
+        axios({
+            method: "POST",
+            url: "http://127.0.0.1:8000/api/register",
+            data:data
+        })
+
+        .then(() => {
+            alert("User Created Successfully")
+        })
     }
+
     return(
         <form className="login-form-container">
-            <input placeholder="Name"></input>
-            <input placeholder="Email"></input>
-            <input placeholder="Password"></input>
-            <button onClick={signUp}>Sign Up</button>
+            <input id="name" placeholder="Name"></input>
+            <input id="email" placeholder="Email"></input>
+            <input id="password" placeholder="Password"></input>
+            <button type="button" onClick={signUp}>Sign Up</button>
             <Link to={"/"}>Log in</Link>
         </form>
     )
