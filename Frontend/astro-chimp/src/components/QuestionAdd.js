7eacf4b80bfb8b1f6c7d712af85a648cc48c217d
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-20 12:09:15 +0300

    Added Linking to Add Question

--- Frontend/astro-chimp/src/components/QuestionAdd.js
+++ Frontend/astro-chimp/src/components/QuestionAdd.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import NavBar from './NavBar';
 import SelectOptions from './SelectOptions';
+import { useRef } from 'react';
+import axios from 'axios';
 
 
 function QuestionAdd() {
+
+    let question_name = useRef(null)
+
+    function addQuestion(){
+        let data = new FormData()
+        data.append("survey_id", localStorage.getItem("survey_id"))
+        data.append("name", question_name.current.value)
+        data.append("type", "type")
+
+        axios({
+            method: "POST",
+            url: "http://127.0.0.1:8000/api/admin/add-question",
+            data:data
+        })
+
+        .then(function(response){
+            console.log(response)
+        })
+    }
+
   return (
     <div>
         <NavBar/>
         <div className="add-question-container">
                 <label>Add Question</label>
-                <input></input>
+                <input ref={question_name}></input>
                 <SelectOptions/>
-        <button>Submit</button>
+        <button onClick={addQuestion}>Submit</button>
         </div>
     </div>
   )
